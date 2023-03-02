49454ceea5de207acabffbc02f41608c7885fcdf
Breno Rocha <brenopenarocha2@gmail.com>
2023-03-02 18:52:38 -0300

    Page initial customization with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React from 'react'
 
 class App extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            input: ''
+        }
+    }
+
+
     render() {
-        return <div></div>
+        return (
+            <div>
+                <h1 className="text-center mt-3 mb-5">Markdown Previewer</h1>
+                <div className="row">
+                    <div className="col-6 d-flex flex-column align-items-center">
+                        <p className="fw-bold">Enter your text</p>
+                        <textarea className="p-3" id="editor" />
+                    </div>
+
+                    <div className="col-6 d-flex flex-column align-items-center">
+                        <p className="fw-bold">Preview</p>
+                        <div className="preview-result p-2" id="preview" />
+                    </div>
+                </div>
+            </div>
+        )
     }
 }
 
