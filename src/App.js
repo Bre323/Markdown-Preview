bc046c8a33aeb5c0a4dd91a874fc503f35d2cdc0
Breno Rocha <brenopenarocha2@gmail.com>
2023-03-03 11:35:51 -0300

    Project Finished

--- src/App.js
+++ src/App.js
@@ -1,4 +1,37 @@
 import React from 'react'
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
+
+const firstInput = 
+`This is a paragraph
+
+**This is a bolded text**
+
+>Block Quotes!
+
+# Heading 1
+## Heading 2
+### Heading 3
+
+- List Item n°1
+- List Item n°2
+- List Item n°3
+
+[Google](https://google.com)
+
+This is a inline code: \`<div></div>\`
+
+This is a block of code:
+\`\`\`
+let x = 1;
+let y = 2;
+let sum = x + y;
+\`\`\`
+
+This is a random image:
+
+![Random image](https://source.unsplash.com/random)
+`
+
 
 class App extends React.Component {
 
@@ -6,24 +39,33 @@ class App extends React.Component {
         super(props)
 
         this.state = {
-            input: ''
+            input: firstInput
         }
     }
 
 
+    handleChange = event => {
+        this.setState({
+            input: event.target.value
+        })
+    }
+
+
     render() {
+        const input = this.state.input
+
         return (
-            <div>
+            <div className="container-md">
                 <h1 className="text-center mt-3 mb-5">Markdown Previewer</h1>
-                <div className="row">
+                <div className="row mb-5">
                     <div className="col-6 d-flex flex-column align-items-center">
                         <p className="fw-bold">Enter your text</p>
-                        <textarea className="p-3" id="editor" />
+                        <textarea className="p-3" id="editor" value={input} onChange={this.handleChange} />
                     </div>
 
                     <div className="col-6 d-flex flex-column align-items-center">
                         <p className="fw-bold">Preview</p>
-                        <div className="preview-result p-2" id="preview" />
+                        <ReactMarkdown className="preview-result p-2" id="preview" children={input} />
                     </div>
                 </div>
             </div>
